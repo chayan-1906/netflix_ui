006543e7f7feb6e1d7b138247002dadb0b423919
pdas9647 <padmanabhadas9647@gmail.com>
2022-10-22 23:48:43 +0530

    main.dart + Home Screen

---
+++ android/app/src/main/kotlin/com/padmanabhasmac/netflix_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.padmanabhasmac.netflix_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Netflix UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
